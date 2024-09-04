5566674689ecc3a699f103fe8f70c04274ebe131
Syed Ikramullah <154834130+syed-ikramullah@users.noreply.github.com>
2024-09-04 23:11:17 +0500

    Add files via upload

--- Functions/Task1.cpp
+++ Functions/Task1.cpp
@@ -1,45 +1,20 @@
-/*
 #include<iostream>
 using namespace std;
-int add(int, int);
-int sub(int, int);
-int mult(int, int);
-float divi(float, float);
-
+void reverse(float[], int);
 int main() {
 
-	int num1, num2;
+	float arr[5] = { 1.2,2.3,3.6,4.9,5.1 };
 
-	cout << "Enter number 1: ";
-	cin >> num1;
-	cout << "Enter number 2: ";
-	cin >> num2;
 
-	cout << "Sum of the two numbers is: " << add(num1, num2) << endl;
-	cout << "Subtraction of the two numbers is: " << sub(num1, num2) << endl;
-	cout << "Multiplication of the two numbers is: " << mult(num1, num2) << endl;
-	cout << "Division of the two numbers is: " << divi(num1, num2) << endl;
 
 	system("pause");
 	return 0;
 }
+void reverse(float arr[], int tot) {
 
-int add(int a, int b) {
+	
 
-	int c = a + b;
-	return c;
+	for (int i = ; i >= 0; i--) {
+		cout << arr[i] << " ";
+	}
 }
-int sub(int a, int b) {
-	int c = a - b;
-	return c;
-}
-int mult(int a, int b) {
-
-	int c = a * b;
-	return c;
-}
-float divi(float a, float b) {
-	float c = a / b;
-	return c;
-}
-*/
